4911bac4f928b72387cfa329fb94d11c39d32fc4
Hector Bonilla <bonilla.osorio0@gmail.com>
2021-11-25 19:34:25 -0600

    Agregar Autenticacion JWT

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.onload = estaAutenticado();
+
+function estaAutenticado(){
+    if(localStorage.getItem("token") != null){
+        console.log("autenticado");
+    }else{
+        document.location.href="../login.html";
+    }
+}
+
+const toggleButton  = document.getElementById('toggle-button');
+const navilist = document.getElementById('navi-list');
+
+const cerrarSesionButton = document.getElementById('cerrarSesion-button');
+
+toggleButton.addEventListener('click', ()=>{
+    navilist.classList.toggle('active');
+});
+
+cerrarSesionButton.addEventListener('click',()=>{
+    localStorage.removeItem('token');
+    document.location.href="../login.html";
+});
